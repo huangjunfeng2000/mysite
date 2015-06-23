fa411cbb4eac74e95b850ab0ef0bbcf6cf1e58b8
hfq <hfq@gptoil.com>
2015-06-23 14:25:04 +0800

    Add datetime code

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from views import hello
+from views import hello, current_datetime
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', hello),
+    url(r'^time/$', current_datetime),
 ]
