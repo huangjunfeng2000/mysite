322a90e35aac119158bfb36333775ad66056ce0d
hfq <hfq@gptoil.com>
2015-06-24 11:33:49 +0800

    Use temlate simply

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 import datetime 
+from django.template import Template, Context
 
 def hello(request):
 	return HttpResponse("Hello world")
@@ -7,6 +8,8 @@ def hello(request):
 def current_datetime(request): 
 	now = datetime.datetime.now() 
 	html = "<html><body>It is now %s.</body></html>" % now 
+	t = Template("<html><body>It is now2 {{ current_date }}.</body></html>")
+	html = t.render(Context({'current_date' : now}))
 	return HttpResponse(html)
 
 def hours_ahead(request, offset):
@@ -14,6 +17,8 @@ def hours_ahead(request, offset):
 		offset = int(offset)
 	except ValueError:
 		raise Http404()
+
+	assert False
 
 	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
 	html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
